684fb1359459a23bb157c5d7419ec9929335d77a
lauco <lauco@192.168.12.138>
2022-11-14 14:34:39 +0100

    He creado el constructor

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
@@ -1,6 +1,12 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 public enum Color {
-BLANCO, NEGRO;
-String cadenaAMostrar;
+	BLANCO("Blanco"), NEGRO("Negro");
+
+	String cadenaAMostrar;
+
+	private Color(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+
 }
