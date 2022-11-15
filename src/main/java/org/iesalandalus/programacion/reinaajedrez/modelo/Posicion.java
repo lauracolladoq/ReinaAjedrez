8b9217138bdb0366e7d4d5d91831b67632541e4f
lauco <lauco@192.168.12.138>
2022-11-15 12:18:21 +0100

    Corección del toString

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -65,7 +65,7 @@ public class Posicion {
 
 	@Override
 	public String toString() {
-		return String.format("fila=valorFila, columna=valorColumna", fila, columna);
+		return String.format("fila=%s, columna=%s", fila, columna);
 	}
 
 }
