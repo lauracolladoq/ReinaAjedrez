83ab3e58fef9d7db01d38b3c7bdd16e3aa3ad56d
lauco <lauco@192.168.12.138>
2022-11-15 12:25:56 +0100

    He creados los get y set de los atributos

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -5,4 +5,17 @@ public class Reina {
 	private Color color;
 	private Posicion posicion;
 	
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
 }
