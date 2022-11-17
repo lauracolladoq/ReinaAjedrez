aceaffc197e63e7aaee38332d330da5ef66d7e1d
lauco <lauco@192.168.12.138>
2022-11-17 14:08:24 +0100

    Creación del método mover

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -34,6 +34,44 @@ public class Reina {
 		} else if (color == Color.NEGRO) {
 			posicion = new Posicion(8, 'd');
 		}
+	}
+
+	public void mover(Direccion direccion, int pasos) {
+		if (direccion == null) {
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+		if (pasos < 1 || pasos > 7) {
+			throw new IllegalArgumentException("ERROR: El número de pasos debe estar comprendido entre 1 y 7.");
+		} else {
+			switch (direccion) {
+			case NORTE:
+				this.posicion = new Posicion(this.posicion.getFila() + pasos, this.posicion.getColumna());
+				break;
+			case NORESTE:
+				this.posicion = new Posicion(this.posicion.getFila() + pasos, (char) (this.posicion.getColumna() + pasos));
+				break;
+			case ESTE:
+				this.posicion = new Posicion(this.posicion.getFila(), (char) (this.posicion.getColumna() + pasos));
+				break;
+			case SURESTE:
+				this.posicion = new Posicion(this.posicion.getFila() - pasos, (char) (this.posicion.getColumna() + pasos));
+				break;
+			case SUR:
+				this.posicion = new Posicion(this.posicion.getFila() - pasos, this.posicion.getColumna());
+				break;
+			case SUROESTE:
+				this.posicion = new Posicion(this.posicion.getFila() - pasos, (char) (this.posicion.getColumna() + pasos));
+				break;
+			case OESTE:
+				this.posicion = new Posicion(this.posicion.getFila(), (char) (this.posicion.getColumna() - pasos));
+				break;
+			case NOROESTE:
+				this.posicion = new Posicion(this.posicion.getFila() + pasos, (char) (this.posicion.getColumna() - pasos));
+				break;
+
+			}
+
+		}
 
 	}
 
