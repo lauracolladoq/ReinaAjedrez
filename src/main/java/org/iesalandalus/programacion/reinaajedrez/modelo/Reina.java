e735a0d43d8b2c6e5a2e6e2104f4bb3ab38f494d
lauco <lauco@192.168.12.138>
2022-11-17 13:07:48 +0100

    Edición constructor por defecto

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,29 +1,29 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Reina {
-	
+
 	private Color color;
 	private Posicion posicion;
-	
+
 	public Color getColor() {
 		return color;
 	}
+
 	private void setColor(Color color) {
 		this.color = color;
 	}
+
 	public Posicion getPosicion() {
 		return posicion;
 	}
+
 	private void setPosicion(Posicion posicion) {
 		this.posicion = posicion;
 	}
-	
-	public Reina () {
+
+	public Reina() {
 		color = Color.BLANCO;
-		posicion = new Posicion (1, 'd');
+		posicion = new Posicion(1, 'd');
 	}
 
-
-	
-	
 }
