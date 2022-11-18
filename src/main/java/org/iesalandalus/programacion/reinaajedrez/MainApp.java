db0c091de2a38d90438046ea098b92f65d4d2a17
lauco <lauco@192.168.12.138>
2022-11-18 08:38:25 +0100

    Creación atributo reina

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -3,6 +3,6 @@ package org.iesalandalus.programacion.reinaajedrez;
 public class MainApp {
 	
 	public static void main(String[] args) {
-		System.out.println(Consola.elegirPasos());
+	int reina;
 	}
 }
